98a65ec7c5e746252e65b91f45517702b07f1f9c
isabellyzaghi <159458058+isabellyzaghi@users.noreply.github.com>
2024-10-09 11:02:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,62 @@
+criaCartao(
+    'medicina',
+    'qual o maior orgao do corpo humano',
+    'O maior orgao do ser humano e a pele?'
+)
+
+criaCartao(
+    'matematica',
+    'Qual e o valor de PI?',
+    'O valor de PI e 3,14'
+)
+
+criaCartao(
+    'biologia',
+    'Qual o maior animal mamifero?',
+    'A baleia'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz correr em Inglês?',
+    'Correr em ingles é RUN'
+)
+
+criaCartao(
+    'biologia',
+    'Que animal e comumente chamado de rei da selva?',
+    'O leao'
+)
+
+criaCartao(
+    'biologia',
+    'Qual o maior animal terrestre?',
+    'O elefante'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz comer em Inglês?',
+    'Correr em ingles é EAT'
+    
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'Qual é a função do pronome pessoal na frase "Ela foi ao mercado e ele ficou em casa"??',
+    'O pronome pessoal indica os sujeitos da ação.'
+    
+)
+
+criaCartao(
+    'ciencias',
+    'O que é fotossíntese e qual é a sua importância para os seres vivos?',
+    'Fotossíntese é a conversão de luz solar em energia e oxigênio, essencial para a vida na Terra.'
+  )  
+
+  criaCartao(
+    'geografia',
+    'Qual o maior pais do mundo?',
+    'Russia.'
+  )  
+
